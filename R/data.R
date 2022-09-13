08836d056fc74f2f5c9ea5ee7c95e62d38ac8718
ChrisMitchell42 <chris.mitchell@cantab.net>
2022-09-13 15:26:42 +0100

    data files

---
+++ R/data.R
@@ -0,0 +1,11 @@
+# 1. questions
+
+#' Student Authored Questions from 2020 - 2021
+#'
+#' Description of the dataset : 1921 questions and their unique IDs
+#'
+#' @format one object of class data.frame
+#'
+#' @source PeerWise
+#' @name questions
+NULL
